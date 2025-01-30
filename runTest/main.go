a762726916aa25fb2da16ae0a5c94267efe2b9d5
gonyango <grantononyango@gmail.com>
2025-01-30 12:01:38 +0300

    feat: implemented simulatepath func

--- runTest/main.go
+++ runTest/main.go
@@ -5,20 +5,37 @@ import (
 	"lem-in/functions"
 )
 
-func main(){
-	adjList := map[string][]string{
-		"A": {"B", "C"},
-		"B": {"A","D","E"},
-		"C":{"A","F"},
-		"D":{"B"},
-		"E":{"B","F"},
-		"F":{"C","E"},
+// func main(){
+// 	adjList := map[string][]string{
+// 		"A": {"B", "C"},
+// 		"B": {"A","D","E"},
+// 		"C":{"A","F"},
+// 		"D":{"B"},
+// 		"E":{"B","F"},
+// 		"F":{"C","E"},
+// 	}
+// 	path := functions.BFS(adjList, "A","F")
+
+// 	paths := [][]string{{"A","B","C"},{"A","D","E","C"},{"A","G","H","F","C"}}
+
+// 	fmt.Println(functions.AllocateAnts(paths, 6))
+
+// 	fmt.Println(path)
+// }
+
+func main() {
+	farm, err := functions.ParseInput("../example.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
-	path := functions.BFS(adjList, "A","F")
 
-	paths := [][]string{{"A","B","C"},{"A","D","E","C"},{"A","G","H","F","C"}}
+	path := functions.BFS(farm.AdjList, farm.Start, farm.End)
+	if path == nil{
+		fmt.Println("No path found")
+		return
+	}
 
-	fmt.Println(functions.AllocateAnts(paths, 6))
-
-	fmt.Println(path)
+	antsPerPath := functions.AllocateAnts([][]string{path}, farm.Ants)
+	functions.SimulateAnts([][]string{path}, antsPerPath)
 }

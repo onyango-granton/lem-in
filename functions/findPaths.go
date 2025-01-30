26eeffcfcd609976c6f43246dc607449b2664fb3
gonyango <grantononyango@gmail.com>
2025-01-30 16:44:42 +0300

    feat: find poaths

--- functions/findPaths.go
+++ functions/findPaths.go
@@ -1,9 +1,13 @@
 package functions
 
-func BFS(adjList map[string][]string, start, end string) []string {
+import "fmt"
+
+func BFS(adjList map[string][]string, start, end string) [][]string {
+	fmt.Println("HERE", adjList)
 	queue  := [][]string{{start}}
 	visited := make(map[string]bool)
 	visited[start] = true
+	var allPaths [][]string
 
 	for len(queue) > 0{
 		path := queue[0]
@@ -11,7 +15,8 @@ func BFS(adjList map[string][]string, start, end string) []string {
 		lastRoom := path[len(path)-1]
 
 		if lastRoom == end{
-			return path
+			allPaths = append(allPaths, path)
+			continue
 		}
 
 		for _, neighbor := range adjList[lastRoom]{
@@ -26,5 +31,5 @@ func BFS(adjList map[string][]string, start, end string) []string {
 			}
 		}
 	}
-	return nil
+	return allPaths
 }

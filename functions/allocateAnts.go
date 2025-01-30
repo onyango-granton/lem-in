ca1400656e24c4cef9272f11fb4f302a8b9b0902
gonyango <grantononyango@gmail.com>
2025-01-30 11:46:32 +0300

    feat: add function to allocate ants to path

---
+++ functions/allocateAnts.go
@@ -0,0 +1,24 @@
+package functions
+
+func AllocateAnts(paths [][]string, ants int) []int {
+	antsPerPath := make([]int, len(paths))
+	pathLengths := make([]int, len(paths))
+
+	for i, path := range paths {
+		pathLengths[i] = len(path)
+	}
+
+	for i := 0; i < ants; i++{
+		minIndex := 0
+		minValue := antsPerPath[0]+pathLengths[0]
+
+		for j:=0; j < len(paths); j++{
+			if antsPerPath[j]+pathLengths[j] < minValue{
+				minIndex = j
+				minValue = antsPerPath[j] + pathLengths[j]
+			}
+		}
+		antsPerPath[minIndex]++
+	}
+	return antsPerPath
+}

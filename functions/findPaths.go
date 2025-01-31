14b5974df5a02bc26cd7ee6bc4fe9a03878d8569
gonyango <grantononyango@gmail.com>
2025-01-31 16:20:59 +0300

    add: implemeted dfs over bfs function

--- functions/findPaths.go
+++ functions/findPaths.go
@@ -1,35 +1,40 @@
 package functions
 
-import "fmt"
+// DFS function to find all paths from start to end
+func DFS(adjList map[string][]string, start, end string) [][]string {
+	visited := make(map[string]bool) // Track visited rooms to avoid cycles
+	var allPaths [][]string          // Store all valid paths
+	var currentPath []string         // Track the current path being explored
 
-func BFS(adjList map[string][]string, start, end string) [][]string {
-	fmt.Println("HERE", adjList)
-	queue  := [][]string{{start}}
-	visited := make(map[string]bool)
-	visited[start] = true
-	var allPaths [][]string
+	// Recursive DFS helper function
+	var dfsHelper func(node string)
+	dfsHelper = func(node string) {
+		// Mark the current node as visited and add it to the current path
+		visited[node] = true
+		currentPath = append(currentPath, node)
 
-	for len(queue) > 0{
-		path := queue[0]
-		queue = queue[1:]
-		lastRoom := path[len(path)-1]
-
-		if lastRoom == end{
-			allPaths = append(allPaths, path)
-			continue
-		}
-
-		for _, neighbor := range adjList[lastRoom]{
-			if !visited[neighbor] {
-				
-				visited[neighbor] = true
-				newPath := append([]string{}, path...)
-				
-				newPath = append(newPath, neighbor)
-				queue = append(queue, newPath)
-				
+		// If the current node is the end, add the current path to allPaths
+		if node == end {
+			// Make a copy of the current path to avoid overwriting
+			pathCopy := make([]string, len(currentPath))
+			copy(pathCopy, currentPath)
+			allPaths = append(allPaths, pathCopy)
+		} else {
+			// Explore all neighbors of the current node
+			for _, neighbor := range adjList[node] {
+				if !visited[neighbor] {
+					dfsHelper(neighbor)
+				}
 			}
 		}
+
+		// Backtrack: Remove the current node from the path and mark it as unvisited
+		currentPath = currentPath[:len(currentPath)-1]
+		visited[node] = false
 	}
+
+	// Start DFS from the start node
+	dfsHelper(start)
+
 	return allPaths
 }

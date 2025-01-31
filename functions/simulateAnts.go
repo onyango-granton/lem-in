2c0006929c24ecfdf4b6b83a6d1a94eda57869ab
gonyango <grantononyango@gmail.com>
2025-01-31 16:38:31 +0300

    feat: improved simulate ant movement function

--- functions/simulateAnts.go
+++ functions/simulateAnts.go
@@ -1,22 +1,51 @@
 package functions
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func SimulateAnts(paths [][]string, antsPerPath []int) {
-	antPositions := make([]int, len(paths))
-	for step := 1; ; step++ {
-		moved := false
-		for i := 0; i < len(paths); i++ {
-			if antPositions[i] < len(paths[i]) && antsPerPath[i] > 0 {
-				fmt.Printf("L%d-%s ", step, paths[i][antPositions[i]])
-				antPositions[i]++
-				antsPerPath[i]--
-				moved = true
+func SimulateAnts(paths [][]string, allocatedAnts []int) {
+	currentAntID := 1
+	antIDs := make([][]int, len(paths))
+	for pathIdx := range paths {
+		antCount := allocatedAnts[pathIdx]
+		antIDs[pathIdx] = make([]int, antCount)
+		for i := 0; i < antCount; i++ {
+			antIDs[pathIdx][i] = currentAntID
+			currentAntID++
+		}
+	}
+
+	// Calculate maximum time required for all ants to finish
+	maxTime := 0
+	for pathIdx := range paths {
+		pathLen := len(paths[pathIdx]) - 1 // exclude 'start'
+		antCount := allocatedAnts[pathIdx]
+		for antInPath := 0; antInPath < antCount; antInPath++ {
+			if t := antInPath + pathLen; t > maxTime {
+				maxTime = t
 			}
 		}
-		if !moved {
-			break
+	}
+
+	// Simulate each time step and collect movements
+	for time := 1; time <= maxTime; time++ {
+		var moves []string
+		for pathIdx := range paths {
+			antCount := allocatedAnts[pathIdx]
+			path := paths[pathIdx]
+			for antInPath := 0; antInPath < antCount; antInPath++ {
+				step := time - antInPath
+				if step < 1 || step >= len(path) {
+					continue // Ant hasn't reached or passed this step
+				}
+				antID := antIDs[pathIdx][antInPath]
+				moves = append(moves, fmt.Sprintf("L%d-%s", antID, path[step]))
+			}
 		}
-		fmt.Println()
+		if len(moves) > 0 {
+			fmt.Println(strings.Join(moves, " "))
+		}
 	}
 }

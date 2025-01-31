90039052e6f3be7ca19c4971bda16adb86ad9ea5
gonyango <grantononyango@gmail.com>
2025-01-31 16:14:09 +0300

    added ilustrator function to main

--- runTest/main.go
+++ runTest/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"lem-in/functions"
 )
@@ -25,7 +26,7 @@ func main() {
 		fmt.Println(path)
 	}
 
-	allocatedAnts,_ := functions.AllocateAnts(allPaths, farm.Ants)
+	allocatedAnts, _ := functions.AllocateAnts(allPaths, farm.Ants)
 	functions.SimulateAnts(allPaths, allocatedAnts)
 	SimulateAnts()
 }
@@ -69,8 +70,7 @@ func DFS(adjList map[string][]string, start, end string) [][]string {
 	return allPaths
 }
 
-
-func SimulateAnts(){
+func SimulateAnts() {
 	farm, err := functions.ParseInput("example.txt")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -89,13 +89,102 @@ func SimulateAnts(){
 		fmt.Println(path)
 	}
 
-	allocatedAnts,paths := functions.AllocateAnts(allPaths, farm.Ants)
-	antID := 0
-	for i, ch := range allocatedAnts{
-		
-		for ant:=0; ant<ch; ant++{
-			antID++
-			fmt.Printf("L%d-path%v\n", antID,paths[i])
+	// get longest path
+	// for i, ch := range allocatedAnts{
+
+	// 	for ant:=0; ant<ch; ant++{
+	// 		antID++
+	// 		antPath[antID]=paths[i]
+	// 	}
+	// }
+
+	// for _,path := range paths{
+	// 	//fmt.Println(index, path)
+	// 	for i, step := range path{
+	// 		for j, _ := range antPath{
+	// 			if i >= len(antPath[j]){
+	// 				break
+	// 			}
+	// 			if step == antPath[j][i]{
+	// 				fmt.Printf("L%v%v",j,step)
+	// 				break
+	// 			}
+	// 		}
+
+	// 	}
+	// }
+
+	
+
+	findPaths()
+}
+
+func findPaths() {
+	farm, err := functions.ParseInput("example2.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Call DFS to find all paths
+	allPaths := DFS(farm.AdjList, farm.Start, farm.End)
+	if len(allPaths) == 0 {
+		fmt.Println("No paths found")
+		return
+	}
+
+	allocatedAnts, paths :=functions.AllocateAnts(allPaths, farm.Ants)
+	// paths := [][]string{
+	// 	{"start", "t", "E", "a", "m", "end"},
+	// 	{"start", "h", "A", "c", "k", "end"},
+	// 	{"start", "h", "n", "e", "end"},
+	// 	{"start", "h", "n", "m", "end"},
+	// 	{"start", "0", "o", "n", "e", "end"},
+	// 	{"start", "0", "o", "n", "m", "end"},
+	// }
+	// allocatedAnts := []int{2, 2, 2, 2, 1, 1}
+
+	// Assign unique IDs to each ant based on path and order
+	currentAntID := 1
+	antIDs := make([][]int, len(paths))
+	for pathIdx := range paths {
+		antCount := allocatedAnts[pathIdx]
+		antIDs[pathIdx] = make([]int, antCount)
+		for i := 0; i < antCount; i++ {
+			antIDs[pathIdx][i] = currentAntID
+			currentAntID++
+		}
+	}
+
+	// Calculate maximum time required for all ants to finish
+	maxTime := 0
+	for pathIdx := range paths {
+		pathLen := len(paths[pathIdx]) - 1 // exclude 'start'
+		antCount := allocatedAnts[pathIdx]
+		for antInPath := 0; antInPath < antCount; antInPath++ {
+			if t := antInPath + pathLen; t > maxTime {
+				maxTime = t
+			}
+		}
+	}
+
+	// Simulate each time step and collect movements
+	for time := 1; time <= maxTime; time++ {
+		var moves []string
+		for pathIdx := range paths {
+			antCount := allocatedAnts[pathIdx]
+			path := paths[pathIdx]
+			for antInPath := 0; antInPath < antCount; antInPath++ {
+				step := time - antInPath
+				if step < 1 || step >= len(path) {
+					continue // Ant hasn't reached or passed this step
+				}
+				antID := antIDs[pathIdx][antInPath]
+				moves = append(moves, fmt.Sprintf("L%d-%s", antID, path[step]))
+			}
+		}
+		if len(moves) > 0 {
+			fmt.Println(strings.Join(moves, " "))
 		}
 	}
 }
